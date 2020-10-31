9b7fef5b1985125a0ae5b5c59847089809e8e0b1
Charing Chen <charinghaxen@gmail.com>
2020-10-30 18:22:11 -0700

    feat: include decel waypoints fix: Add logwarning to print out the status of the light

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -75,7 +75,7 @@ class TLDetector(object):
             msg (Image): image from car-mounted camera
 
         """
-        rospy.logwarn('image_cb called')
+        # rospy.logwarn('image_cb called')
 
         self.has_image = True
         self.camera_image = msg
@@ -122,15 +122,15 @@ class TLDetector(object):
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
 
         """
-        # return light.state
-        if(not self.has_image):
-            self.prev_light_loc = None
-            return False
+        return light.state
+        # if(not self.has_image):
+        #     self.prev_light_loc = None
+        #     return False
 
-        cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, "bgr8")
+        # cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, "bgr8")
 
-        #Get classification
-        return self.light_classifier.get_classification(cv_image)
+        # #Get classification
+        # return self.light_classifier.get_classification(cv_image)
 
     def process_traffic_lights(self):
         """Finds closest visible traffic light, if one exists, and determines its
@@ -164,8 +164,8 @@ class TLDetector(object):
 
         if closest_light:
             state = self.get_light_state(light)
-            rospy.logwarn('Traffic line state{0}'.format(state))
-            
+            rospy.logwarn('Traffic line state {0}'.format(state))
+            rospy.logwarn('Line index {0}'.format(line_wp_idx))
             return line_wp_idx, state
         self.waypoints = None
         return -1, TrafficLight.UNKNOWN
